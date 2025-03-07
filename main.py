d0206cdf68414e29c746e71ef52f4d1a9b37d093
Trần Công Minh <stpd.mnky.114@gmail.com>
2025-03-07 11:14:05 +0700

    Refactor trading module: remove unused files and implement TradingBot class in trading_bot.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-
+import json
+from trading import TradingBot
 
 if __name__ == "__main__":
-    print("Hello World!")
+    with open("api_config.json") as f:
+        config = json.load(f)["test"]
+        
+    bot = TradingBot()
+    bot.run()
